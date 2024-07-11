cdd313ab0f2aea6f229c07ae81070e354b1cdc90
Evan Siroky <evan.siroky@yahoo.com>
2024-07-10 23:16:21 -0700

    WIP refactor downloader to download multiple GTFS files

--- downloader.py
+++ downloader.py
@@ -1,55 +1,155 @@
-### A script continually download a single GTFS-RT URL file and save files to a folder
+### A script to continually download and save multiple GTFS files to folders organized by
+# feed and date
 
-import json
+import logging
 import os
-import requests
+import shutil
 import sys
 import time
-from datetime import datetime, timedelta
 
+from datetime import datetime
+
+import requests
+import schedule
+import pytz
+
+from gtfs_functions import Feed
 from utils import create_folder, load_config
 
-def sleep_until_next_polling_mark(config, next_polling_mark):
-    if not next_polling_mark:
-        next_polling_mark = datetime.now()
-        next_polling_mark = next_polling_mark.replace(microsecond=0)
-        next_polling_mark = next_polling_mark.replace(
-          second=next_polling_mark.second - next_polling_mark.second % config['polling_interval']
-        )
-        next_polling_mark = next_polling_mark + timedelta(seconds=config['polling_interval'])
-    
-    delta = next_polling_mark - datetime.now()
-    sleep_seconds = delta.total_seconds()
-    if sleep_seconds > 0:
-        time.sleep(sleep_seconds)
-    
-    return next_polling_mark + timedelta(seconds=config['polling_interval'])
-    
-def make_request(url, file_path):
+
+feeds = {}
+save_folder = 'saved_data'
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(threadName)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+
+RT_URLS = ['service_alerts_url', 'trip_updates_url', 'vehicle_positions_url']
+
+def download_file(url, save_filename):
+    logging.info(f"Downloading from {url}")
     try:
         response = requests.get(url, stream=True)
         response.raise_for_status()  # Check if the request was successful
 
-        with open(file_path, 'wb') as file:
+        with open(save_filename, 'wb') as file:
             for chunk in response.iter_content(chunk_size=8192):
                 file.write(chunk)
 
-        print(f"File saved successfully to {file_path}")
+        logging.info(f"File saved successfully to {save_filename}")
     except requests.exceptions.RequestException as e:
-        print(f"Failed to download the file: {e}")
+        logging.error(f"Failed to download from URL ({url}): {e}")
+
+
+def download_and_process_config():
+    global feeds, save_folder
+    config = load_config('downloader.py')
+    save_folder = config['save_folder']
+    create_folder(save_folder)
+
+    found_feeds = set()
+    fetch_timezones = set()
+
+    for feed_config in feeds.values():
+        fetch_timezones.add(feed_config['timezone'])
+
+    for name, urls in config['feeds'].items():
+        found_feeds.add(name)
+        if name not in feeds:
+            logging.info(f"Processing new feed: {name}")
+            feeds[name] = {}
+
+            # create feed save folder
+            feed_save_folder = os.path.join(save_folder, name)
+            create_folder(feed_save_folder)
+
+            # download initial schedule feed
+            initial_gtfs_schedule_path = os.path.join(feed_save_folder, 'init_gtfs_schedule.zip')
+            download_file(urls['schedule_url'], initial_gtfs_schedule_path)
+
+            # add feed to the timezone the first agency is associated with
+            feed = Feed(initial_gtfs_schedule_path)
+            print(feed.agency[0])
+            sys.exit()
+            agency_timezone = feed.agency[0].agency_timezone
+            print(agency_timezone)
+            feeds[name]['timezone'] = agency_timezone
+
+            if agency_timezone not in fetch_timezones:
+                # schedule
+                schedule.every().day.at('2:30', agency_timezone).do(
+                    download_schedule_files_for_timezone, timezone=agency_timezone)
+                fetch_timezones.add(agency_timezone)
+
+            # copy initial file to appropriate date folder
+            current_date = datetime.now(pytz.timezone(agency_timezone)).date()
+            schedule_folder = os.path.join(feed_save_folder, current_date, 'schedule')
+            create_folder(schedule_folder)
+            shutil.copy(initial_gtfs_schedule_path, os.path.join(schedule_folder, 'gtfs.zip'))
+
+        feeds[name]['urls'] = urls
+
+    # remove feeds no longer present
+    for feed_name in feeds.keys():
+        if feed_name not in found_feeds:
+            del feeds[feed_name]
+    
+
+def download_rt_files():
+    global feeds, save_folder
+
+    for name, feed_config in feeds.items():
+        now = datetime.now(pytz.timezone(feed_config['timezone']))
+        formatted_request_time = f"{now:%Y-%m-%d-%H-%M-%S}"
+        feed_date = now.date()
+        feed_date_save_folder = os.path.join(save_folder, name, feed_date)
+
+        for url_type in RT_URLS:
+            if url_type in feed_config['urls']:
+                feed_date_url_type_save_folder = os.path.join(feed_date_save_folder, url_type)
+                create_folder(feed_date_url_type_save_folder)
+                download_file(
+                    feed_config['urls'][url_type],
+                    os.path.join(feed_date_url_type_save_folder, f"{formatted_request_time}.pb")
+                )
+
+
+def download_schedule_files_for_timezone(timezone):
+    global feeds, save_folder
+
+    now = datetime.now(pytz.timezone(timezone))
+    feed_date = now.date()
+
+    for name, feed_config in feeds.items():
+        if feed_config['timezone'] != timezone:
+            continue
+
+        feed_date_url_type_save_folder = os.path.join(save_folder, name, feed_date, 'schedule')
+        create_folder(feed_date_url_type_save_folder)
+        download_file(
+            feed_config['urls']['schedule'],
+            os.path.join(feed_date_url_type_save_folder, 'gtfs.zip')
+        )
+
 
 def main():
-    config = load_config('downloader.py')
-    next_polling_mark = None
-    
-    create_folder(config['save_folder'])
+    # first load and process config to save initial schedule files and also schedule future
+    # schedule feed downloads
+    download_and_process_config()
+
+    # schedule rt file downloads for every 20 seconds
+    schedule.every().minute.at(':00').do(download_rt_files)
+    schedule.every().minute.at(':20').do(download_rt_files)
+    schedule.every().minute.at(':40').do(download_rt_files)
+
+    # update config every minute
+    schedule.every().minute.at(':45').do(download_and_process_config)
     
     while True:
-        next_polling_mark = sleep_until_next_polling_mark(config, next_polling_mark)
-        request_time = datetime.now()
-        formatted_request_time = f"{request_time:%Y-%m-%d-%H-%M-%S}"
-        print(f"making request for {formatted_request_time}")
-        make_request(config['url'], os.path.join(config['save_folder'], formatted_request_time))
+        schedule.run_pending()
+        time.sleep(1)
+
 
 if __name__ == '__main__':
     main()
